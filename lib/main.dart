f4ed6748922840072a9c08f9ed93da727524e8f4
GEONWOO <rjsdn736@naver.com>
2023-01-20 18:22:18 +0900

    캘린더 스타일 주기

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:todo/view/MainPage.dart';
-
-void main() {
+import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
+void main() async {
+  //달력 한글로 바꾸기 위해 설정
+  await initializeDateFormatting();
   runApp(const MyApp());
 }
 
@@ -11,20 +14,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+    return GetMaterialApp(
+      title: '건우Todo',
       home: MainPage()
     );
   }
--- lib/view/MainPage.dart
+++ lib/view/MainPage.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:get/get.dart';
+import 'package:todo/main.dart';
 
 class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
+  MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+      child: TableCalendar(
+        //달력 내용
+        focusedDay: DateTime.now(),
+        firstDay: DateTime.utc(2022, 1, 1),
+        lastDay: DateTime(2030, 1, 31),
+        locale: 'ko-KR',
+        //달력 위에 부분 꾸미기
+        headerStyle: HeaderStyle(
+          titleCentered: true,
+          formatButtonVisible: false,
+          titleTextStyle: const TextStyle(
+            fontSize: 20.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // 주말 표시
+          calendarStyle:CalendarStyle(
+            weekendTextStyle: TextStyle(
+            color: Colors.red,
+          ),
+          ),
+
+      ),
+    ));
   }
 }
